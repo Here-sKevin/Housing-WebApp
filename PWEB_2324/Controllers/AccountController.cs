e5f94f02960be976cbac0c6e53de83838f5581b3
Kevin <kevin.rodrigues@devoteam.com>
2023-10-29 01:47:25 +0000

    tp development

---
+++ PWEB_2324/Controllers/AccountController.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using PWEB_2324.Models;
+
+namespace PWEB_2324.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager <IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+        [HttpGet]
+        public IActionResult Login(string url)
+        {
+            return View(new LoginModel()
+            {
+                Url = url
+            });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginModel loginM)
+        {
+            if (!ModelState.IsValid)
+                return View(loginM);
+
+            var user = await _userManager.FindByNameAsync(loginM.UserName);
+
+            if(user != null)
+            {
+                var result = await _signInManager.PasswordSignInAsync(user, loginM.Password, false, false);
+
+                if (result.Succeeded)
+                {
+                    if (string.IsNullOrEmpty(loginM.Url))
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+                    return Redirect(loginM.Url);
+                }
+            }
+            ModelState.AddModelError("", "Login falhou!");
+            return View(loginM);
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(LoginModel registroM)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new IdentityUser { UserName = registroM.UserName };
+                var result = await _userManager.CreateAsync(user, registroM.Password);
+
+                if (result.Succeeded)
+                {
+                    //await _signInManager.SignInAsync(user, isPersistent: false);
+                    return RedirectToAction("Login", "Account");
+                }
+                else
+                {
+                    this.ModelState.AddModelError("Registro", "Falha ao registrar o usuário");
+                }
+            }
+            return View(registroM);
+        }
+    }
+}
--- PWEB_2324/Data/ApplicationDbContext.cs
+++ PWEB_2324/Data/ApplicationDbContext.cs
@@ -1,13 +1,16 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using PWEB_2324.Models;
 
 namespace PWEB_2324.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
         }
+        public DbSet<ImoveisModel> Imoveil { get; set; }
     }
 }
